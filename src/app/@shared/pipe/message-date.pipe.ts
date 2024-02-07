46a61637caa8a4a3e16225be9652d0f3870106f0
Swapnil Opash <sra.opash@gmail.com>
2024-02-07 18:35:07 +0530

    add pipe

---
+++ src/app/@shared/pipe/message-date.pipe.ts
@@ -0,0 +1,35 @@
+import { Pipe, PipeTransform } from '@angular/core';
+
+@Pipe({
+  name: 'messageDate'
+})
+export class MessageDatePipe implements PipeTransform {
+  transform(messages: any[]): any[] {
+    const groupedMessages: any[] = [];
+
+    messages.forEach((message, index) => {
+      const messageDate = new Date(message.createdDate);
+      const today = new Date();
+      const yesterday = new Date(today);
+      yesterday.setDate(today.getDate() - 1);
+
+      let groupHeader = '';
+
+      if (messageDate.toDateString() === today.toDateString()) {
+        groupHeader = 'Today';
+      } else if (messageDate.toDateString() === yesterday.toDateString()) {
+        groupHeader = 'Yesterday';
+      } else {
+        groupHeader = messageDate.toLocaleDateString(); // or any other desired date format
+      }
+
+      if (index === 0 || messageDate.toDateString() !== new Date(messages[index - 1].createdDate).toDateString()) {
+        groupedMessages.push({ date: groupHeader, messages: [message] });
+      } else {
+        groupedMessages[groupedMessages.length - 1].messages.push(message);
+      }
+    });
+    console.log(groupedMessages)
+    return groupedMessages;
+  }
+}
