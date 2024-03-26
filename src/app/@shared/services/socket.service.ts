81494af88b19a873ed7aef481aa8f95b6355a412
Swapnil Opash <sra.opash@gmail.com>
2024-03-26 12:42:43 +0530

    fixed issue

--- src/app/@shared/services/socket.service.ts
+++ src/app/@shared/services/socket.service.ts
@@ -37,21 +37,17 @@ export class SocketService {
         const customHeaders = {
           Authorization: `Bearer ${token}`,
         };
-        // if (this.socket) {
-        //   this.socket?.close();
-        // }
-        if (!this.socket) {
-          this.socket = io(environment.socketUrl, {
-            reconnectionDelay: 100,
-            reconnectionDelayMax: 300,
-            reconnection: true,
-            randomizationFactor: 0.2,
-            // timeout: 120000,
-            reconnectionAttempts: 50000,
-            transports: ['websocket'],
-            auth: customHeaders,
-          });
-        }
+        const socketUrl = environment.socketUrl;
+        this.socket = io(socketUrl, {
+          reconnectionDelay: 100,
+          reconnectionDelayMax: 300,
+          reconnection: true,
+          randomizationFactor: 0.2,
+          // timeout: 120000,
+          reconnectionAttempts: 50000,
+          transports: ['websocket'],
+          auth: customHeaders,
+        });
       }
     }
   }
--- src/app/layouts/auth-layout/pages/login/login.component.ts
+++ src/app/layouts/auth-layout/pages/login/login.component.ts
@@ -131,9 +131,6 @@ export class LoginComponent implements OnInit, AfterViewInit {
             this.sharedService.getUserDetails();
             this.isLoginFailed = false;
             this.isLoggedIn = true;
-            if (this.socketService.socket?.connected) {
-              this.socketService.socket.close();
-            }
             this.socketService.connect();
             this.socketService.socket?.emit('online-users');
             this.socketService?.socket?.on('get-users', (data) => {
