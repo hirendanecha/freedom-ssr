3a4e7d40743b3ce94b2b5ad0bfa4c49395c09f21
ppa-opash <ppa.opash@gmail.com>
2024-04-19 11:25:52 +0530

    Add apk

--- src/app/layouts/main-layout/pages/profile-chats/profile-chats-list/profile-chats-list.component.ts
+++ src/app/layouts/main-layout/pages/profile-chats/profile-chats-list/profile-chats-list.component.ts
@@ -648,7 +648,8 @@ export class ProfileChatsListComponent
       media.endsWith('.docx') ||
       media.endsWith('.xls') ||
       media.endsWith('.xlsx') ||
-      media.endsWith('.zip');
+      media.endsWith('.zip') ||
+      media.endsWith('.apk')
     return media && fileType;
   }
 
@@ -657,7 +658,7 @@ export class ProfileChatsListComponent
   }
 
   isFile(media: string): boolean {
-    const FILE_EXTENSIONS = ['.pdf', '.doc', '.docx', '.xls', '.xlsx', '.zip'];
+    const FILE_EXTENSIONS = ['.pdf', '.doc', '.docx', '.xls', '.xlsx', '.zip','.apk'];
     return FILE_EXTENSIONS.some((ext) => media?.endsWith(ext));
   }
 
