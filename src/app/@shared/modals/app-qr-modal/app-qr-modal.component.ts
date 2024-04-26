7811c8733cfd75a0d57a53a37a629bdef50cfd50
ppa-opash <ppa.opash@gmail.com>
2024-04-26 18:00:35 +0530

    Chnage Qr code UI

--- src/app/@shared/modals/app-qr-modal/app-qr-modal.component.ts
+++ src/app/@shared/modals/app-qr-modal/app-qr-modal.component.ts
@@ -11,7 +11,16 @@ export class AppQrModalComponent {
   @Input() store: string;
   @Input() image: string;
   @Input() title:string | undefined = 'Buzz Ring App'
+  showPlayQr :boolean = false
+  showStoreQr :boolean = false
 
   constructor(public activeModal: NgbActiveModal) {}
   
+  togglePlayApp(){
+    this.showPlayQr = !this.showPlayQr
+  }
+
+  toggleStoreApp(){
+    this.showStoreQr = !this.showStoreQr
+  }
 }
