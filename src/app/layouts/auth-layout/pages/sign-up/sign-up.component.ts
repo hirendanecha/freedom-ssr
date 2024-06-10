cf53f30b7137f9f56978498f069a6707f0bc6cab
vatsal <vso.opash@gmail.com>
2024-06-10 17:02:24 +0530

    Change state input field to select option

--- src/app/layouts/auth-layout/pages/sign-up/sign-up.component.ts
+++ src/app/layouts/auth-layout/pages/sign-up/sign-up.component.ts
@@ -35,6 +35,7 @@ export class SignUpComponent implements OnInit, AfterViewInit {
   allCountryData: any;
   type = 'danger';
   defaultCountry = 'US';
+  allStateData: any;
   profilePic: string;
   profileImg: any = {
     file: null,
@@ -275,6 +276,26 @@ export class SignUpComponent implements OnInit, AfterViewInit {
         this.spinner.hide();
         this.allCountryData = result;
         this.registerForm.get('Zip').enable();
+        this.getAllState(this.defaultCountry)
+      },
+      error: (error) => {
+        this.spinner.hide();
+        console.log(error);
+      },
+    });
+  }
+
+  onCountryChange(event: Event): void {
+    const target = event.target as HTMLSelectElement;
+    this.getAllState(target.value);
+  }
+  
+  getAllState(selectCountry) {
+    // this.spinner.show();
+    this.customerService.getStateData(selectCountry).subscribe({
+      next: (result) => {
+        this.spinner.hide();
+        this.allStateData = result;
       },
       error: (error) => {
         this.spinner.hide();
