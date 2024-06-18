f0dd37b53804fab36b98c789c15511cbf9fd4c4c
vatsal <vso.opash@gmail.com>
2024-06-18 18:20:24 +0530

    Fix search pipe

--- src/app/@shared/pipe/hightlight-text.pipe.ts
+++ src/app/@shared/pipe/hightlight-text.pipe.ts
@@ -1,3 +1,19 @@
+// import { Pipe, PipeTransform } from '@angular/core';
+
+// @Pipe({
+//   name: 'highlight',
+// })
+// export class HighlightPipe implements PipeTransform {
+//   transform(text: string, search: string): string {
+//     if (!search) {
+//       return text;
+//     }
+//     const regex = new RegExp(`(${search})`, 'gi');
+//     return text.replace(regex, '<span class="highlight">$1</span>');
+//   }
+// }
+
+
 import { Pipe, PipeTransform } from '@angular/core';
 
 @Pipe({
@@ -8,7 +24,23 @@ export class HighlightPipe implements PipeTransform {
     if (!search) {
       return text;
     }
-    const regex = new RegExp(`(${search})`, 'gi');
-    return text.replace(regex, '<span class="highlight">$1</span>');
+    
+    const parser = new DOMParser();
+    const doc = parser.parseFromString(text, 'text/html');
+
+    this.highlightTextNodes(doc.body, search);
+
+    return doc.body.innerHTML;
+  }
+
+  private highlightTextNodes(element: Node, search: string) {
+    if (element.nodeType === Node.TEXT_NODE) {
+      const regex = new RegExp(`(${search})`, 'gi');
+      const wrapper = document.createElement('span');
+      wrapper.innerHTML = element.nodeValue.replace(regex, '<span class="highlight">$1</span>');
+      element.parentNode.replaceChild(wrapper, element);
+    } else {
+      element.childNodes.forEach(child => this.highlightTextNodes(child, search));
+    }
   }
 }
