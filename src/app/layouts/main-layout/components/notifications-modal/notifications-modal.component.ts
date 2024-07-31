6b80e3222cb28c0380e5e0e302efbd48c207b7b0
vatsal <vso.opash@gmail.com>
2024-07-31 16:48:06 +0530

    allow notification open in new tab

--- src/app/layouts/main-layout/components/notifications-modal/notifications-modal.component.ts
+++ src/app/layouts/main-layout/components/notifications-modal/notifications-modal.component.ts
@@ -34,16 +34,16 @@ export class NotificationsModalComponent implements AfterViewInit {
       .readUnreadNotification(notification.id, 'Y')
       .subscribe({
         next: (res) => {
-          const type = ['M', 'SC', 'DC', 'VC']
-          if (type.includes(notification?.actionType)) {
-            this.router.navigate([`profile-chats`]);
-          } else {
-            this.router.navigate([`post/${postId}`]);
-          }
+          // const type = ['M', 'SC', 'DC', 'VC']
+          // if (type.includes(notification?.actionType)) {
+          //   this.router.navigate([`profile-chats`]);
+          // } else {
+          //   this.router.navigate([`post/${postId}`]);
+          // }
           // window.open(`post/${postId}`.toString(), '_blank')
-          this.closeModal();
         },
       });
+      this.closeModal();
   }
 
   closeModal(): void {
