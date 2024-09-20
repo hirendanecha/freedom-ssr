3dd48ba82f8ccad29236761cce70537ccd7df323
vatsal <vso.opash@gmail.com>
2024-09-20 11:24:49 +0530

    improve post description

--- src/app/@shared/pipe/post-description.pipe.ts
+++ src/app/@shared/pipe/post-description.pipe.ts
@@ -21,25 +21,54 @@ export class StripHtmlPipe implements PipeTransform {
   //   }
   //   return div.innerText || div.textContent || '';
   // }
+  // convert into single tag
+  // transform(value: string): string {
+  //   const div = document.createElement('div');
+  //   div.innerHTML = value;
+  //   const processNode = (node: Node): string => {
+  //     if (node.nodeType === Node.ELEMENT_NODE) {
+  //       const element = node as HTMLElement;
+  //       if (element.tagName.toLowerCase() === 'a' && element.hasAttribute('data-id')) {
+  //         return element.outerHTML;
+  //       } else if (element.tagName.toLowerCase() === 'br') {
+  //         return '\n';
+  //       } else {
+  //         return Array.from(element.childNodes).map(processNode).join('');
+  //       }
+  //     } else if (node.nodeType === Node.TEXT_NODE) {
+  //       return node.textContent || '';
+  //     }
+  //     return '';
+  //   };
+  //   const result = processNode(div);
+  //   return result || div.textContent || '';
+  // }
   transform(value: string): string {
     const div = document.createElement('div');
     div.innerHTML = value;
     const processNode = (node: Node): string => {
       if (node.nodeType === Node.ELEMENT_NODE) {
         const element = node as HTMLElement;
-        if (element.tagName.toLowerCase() === 'a' && element.hasAttribute('data-id')) {
+        const tagName = element.tagName.toLowerCase();
+        if (tagName === 'a' && element.hasAttribute('data-id')) {
           return element.outerHTML;
-        } else if (element.tagName.toLowerCase() === 'br') {
+        } else if (tagName.toLowerCase() === 'br') {
           return '\n';
-        } else {
-          return Array.from(element.childNodes).map(processNode).join('');
         }
+        const childContent = Array.from(element.childNodes).map(processNode).join('');
+        if (element.childNodes.length === 1 && element.firstChild?.nodeType === Node.ELEMENT_NODE) {
+          const firstChildElement = element.firstChild as HTMLElement;
+          if (firstChildElement.tagName.toLowerCase() === tagName) {
+            return childContent;
+          }
+        }
+        return `<${tagName}>${childContent}</${tagName}>`;
       } else if (node.nodeType === Node.TEXT_NODE) {
         return node.textContent || '';
       }
       return '';
     };
     const result = processNode(div);
-    return result || div.textContent || '';
+    return result;
   }
 }
