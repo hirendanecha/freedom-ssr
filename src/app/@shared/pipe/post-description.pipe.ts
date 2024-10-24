c81dde64841a5758938471cc2148ec01fd91626a
vatsal <vso.opash@gmail.com>
2024-10-24 16:08:17 +0530

    improve post description pipe

--- src/app/@shared/pipe/post-description.pipe.ts
+++ src/app/@shared/pipe/post-description.pipe.ts
@@ -14,36 +14,25 @@ export class TruncatePipe implements PipeTransform {
 })
 export class StripHtmlPipe implements PipeTransform {
   transform(value: string): string {
-    const div = document.createElement('div');
-    div.innerHTML = value;
-    const processNode = (node: Node): string => {
-      if (node.nodeType === Node.ELEMENT_NODE) {
-        const element = node as HTMLElement;
-        const tagName = element.tagName.toLowerCase();
-        if (tagName === 'a' && element.hasAttribute('data-id')) {
-          return element.outerHTML;
-        }
-        Array.from(element.attributes).forEach(attr => element.removeAttribute(attr.name));
-        const childContent = Array.from(element.childNodes)
-          .map(processNode)
-          .join('');
-        if (tagName === 'br') {
-          return '<br>';
-        }
-        if (tagName === 'div') {
-          return `<div>${childContent}</div>`;
-        }
-        return `<${tagName}>${childContent}</${tagName}>`;
-      } else if (node.nodeType === Node.TEXT_NODE) {
-        return node.textContent || '';
-      }
-      return '';
-    };
-    const result = processNode(div);
-    const normalizedResult = result
+    return `${value}`
+      .replace(/<div[^>]*>\s*/gi, '<div>')
+      .replace(/<br[^>]*>\s*/gi, '<br>')
+      .replace(/(<br\s*\/?>\s*){2,}/gi, '<br>')
       .replace(/(?:<div><br><\/div>\s*)+/gi, '<div><br></div>')
-      .replace(/(\n\s*)+/g, '\n')
+      .replace(/<a\s+([^>]*?)>/gi, (match, p1) => {
+        const hrefMatch = p1.match(/\bhref=["'][^"']*["']/);
+        const classMatch = p1.match(/\bclass=["'][^"']*["']/);
+        const dataIdMatch = p1.match(/\bdata-id=["'][^"']*["']/);
+        let allowedAttrs = '';
+        if (hrefMatch) allowedAttrs += ` ${hrefMatch[0]}`;
+        if (classMatch) allowedAttrs += ` ${classMatch[0]}`;
+        if (dataIdMatch) allowedAttrs += ` ${dataIdMatch[0]}`;
+        return `<a${allowedAttrs}>`;
+      })
+      .replace(/<\/?[^>]+(>|$)/gi, (match) => {
+        return /<\/?(a|br|div)(\s+[^>]*)?>/i.test(match) ? match : '';
+      })
+      .replace(/^(?:&nbsp;|\s)+/g, '')
       .trim();
-    return normalizedResult;
   }
 }
