ad567c4244cff242cb4f04c8fe1e8f833a8a4591
vatsal <vso.opash@gmail.com>
2024-11-25 18:24:05 +0530

    Make post and comment multiple link clickable

---
+++ src/app/@shared/pipe/linkify.pipe.ts
@@ -0,0 +1,23 @@
+import { Pipe, PipeTransform } from '@angular/core';
+
+@Pipe({
+  name: 'linkify'
+})
+export class LinkifyPipe implements PipeTransform {
+
+  transform(value: string): string {
+    if (!value) return value;
+
+    const urlRegex = /(?:https?:\/\/|www\.)[^\s<&]+(?:\.[^\s<&]+)+(?:\.[^\s<]+)?/g;
+    return value.replace(urlRegex, (url) => {
+      if (url.startsWith('www') && !/^https?:\/\//.test(url)) {
+        url = 'http://' + url;
+      }
+      const gif = url.trim().replace(/["']/g, '');
+      if (gif.endsWith('.gif')) {
+        return url;
+      }
+      return `<a href="${url}" target="_blank">${url}</a>`;
+    });
+  }
+}
